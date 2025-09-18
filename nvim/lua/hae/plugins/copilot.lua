4e97c5bfc2a864a1943152df5e1419d4f1d0f526
haeohreumkim <z5480978@ad.unsw.edu.au>
2025-09-18 12:18:35 +1000

    new: i hate neovim;

--- nvim/lua/hae/plugins/copilot.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'github/copilot.vim',
-    lazy = false,
-    config = function ()
-        vim.g.copilot_enabled = false 
-    end,
-    -- use opts = {} for passing setup options
-    -- this is equalent to setup({}) function
-}
